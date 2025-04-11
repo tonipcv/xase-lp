538afd95ce1dd0156bbdfd5460307bd6ee325605
tonipcv <xppsalvador@gmail.com>
2025-04-11 18:04:35 -0300

    dsadas

--- src/app/api/messages/route.ts
+++ src/app/api/messages/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server'
 import { prisma } from '@/lib/prisma'
 
+export const runtime = 'edge'
+
 export async function POST(request: Request) {
   try {
     const body = await request.json()
