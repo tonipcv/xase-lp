41f1b44c9e5f55325210c972368142e952d326fb
tonipcv <xppsalvador@gmail.com>
2026-01-21 16:30:49 -0300

    correto no mobile

--- src/components/docs/DocsContent.tsx
+++ src/components/docs/DocsContent.tsx
@@ -9,7 +9,7 @@ export default function DocsContent({ children }: DocsContentProps) {
   return (
     <div className="flex min-h-screen">
       <Sidebar />
-      <main className="flex-1 w-full md:w-auto px-6 md:px-12 py-8 md:py-12 max-w-full md:max-w-[800px]">
+      <main className="flex-1 w-full md:w-auto px-6 md:px-12 pt-20 pb-8 md:py-12 max-w-full md:max-w-[800px]">
         {children}
       </main>
     </div>
